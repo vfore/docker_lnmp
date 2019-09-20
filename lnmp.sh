d91c3adac530e88bec47724e66f9bbb79a06ca38
vfore <timesdev@163.com>
2019-09-20 15:34:30 +0800

    1. nginx、php镜像使用alpine 2. 修改映射目录 3. 修改Dockerfile 4. 修改安装脚本

--- lnmp.sh
+++ lnmp.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-mkdir -p /home/server/mysql/conf.d
-cp ./mysql/my.cnf /home/server/mysql/
-cp ./mysql/conf.d/* /home/server/mysql/conf.d/
+mkdir -p /home/server/mysql/8/conf.d
+cp ./mysql/8/my.cnf /home/server/mysql/8/
+cp ./mysql/8/conf.d/* /home/server/mysql/8/conf.d/
 
 mkdir -p /home/server/nginx/conf.d
 cp ./nginx/nginx.conf /home/server/nginx/
 cp ./nginx/conf.d/* /home/server/nginx/conf.d/
 
-mkdir /home/server/php
-cp ./php-fpm/php.ini /home/server/php/
+mkdir -p /home/server/php/7.3
+cp ./php-fpm/7.3/php.ini /home/server/php/7.3
 
 docker-compose up -d | tee -a ./lnmp_install.log
 docker-compose restart
